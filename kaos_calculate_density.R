c61264a16ed6e2e72c37b8a5a46d2465d8dbe86a
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-10-27 15:08:52 +1100

    Combines all extracted kaos data (one file for each day) and calculates density and combines into one file.

--- kaos_calculate_density.R
+++ kaos_calculate_density.R
@@ -19,6 +19,11 @@ for (i in 1:length(survey.dates)) {
   #sort by interval
   acoustic_38 <- acoustic_38[order(acoustic_38$Interval), ]
   acoustic_120 <- acoustic_120[order(acoustic_120$Interval), ]
+  acoustic_120 <- acoustic_120[c(1:nrow(acoustic_38)), ]
+  
+  #remove layers of -1 
+  acoustic_38 <- acoustic_38[acoustic_38$Layer > 0, ]
+  acoustic_120 <- acoustic_120[acoustic_120$Layer > 0, ]  
   
   #calculate 120kHz - 38kHz for each 10x50 window
   sv_38 <- acoustic_38$Sv_mean
@@ -27,12 +32,12 @@ for (i in 1:length(survey.dates)) {
   sv_120[sv_120 > 500 | sv_120 < -500] <- NA
   sv_diff <- sv_120 - sv_38
   
-  time  <- as.character(acoustic_38$Time_M[seq(1, nrow(acoustic_38), by = 50)])
-  date  <- acoustic_38$Date_M[seq(1, nrow(acoustic_38), by = 50)]
-  lat  <- acoustic_38$Lat_S[seq(1, nrow(acoustic_38), by = 50)]
-  long  <- acoustic_38$Lon_E[seq(1, nrow(acoustic_38), by = 50)]
-  start_time  <- as.character(acoustic_38$Time_S[seq(1, nrow(acoustic_38), by = 50)])
-  end_time  <- as.character(acoustic_38$Time_E[seq(1, nrow(acoustic_38), by = 50)])
+  time  <- as.character(acoustic_38$Time_M[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))])
+  date  <- acoustic_38$Date_M[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))]
+  lat  <- acoustic_38$Lat_S[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))]
+  long  <- acoustic_38$Lon_E[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))]
+  start_time  <- as.character(acoustic_38$Time_S[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))])
+  end_time  <- as.character(acoustic_38$Time_E[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))])
   
   #remove 120 - 38 kHz values outside of [1.02, 14.75] because these are unlikely to be krill
   #dB difference window is from Potts AAD report for KAOS data
@@ -41,7 +46,7 @@ for (i in 1:length(survey.dates)) {
   
   sv <- 10^(sv_120/10)
   
-  mvbs  <- 10*log10(aggregate(matrix(sv, ncol = 1), by = list(rep(c(1:(length(sv)/50)), each = 50)), sum, na.rm = T)$V1/50)
+  mvbs  <- 10*log10(aggregate(matrix(sv, ncol = 1), by = list(rep(c(1:(length(sv)/max(acoustic_38$Layer))), each = max(acoustic_38$Layer))), sum, na.rm = T)$V1/max(acoustic_38$Layer))
   mvbs[mvbs == -Inf] <- NA
   
   #convert to density using target strength (kg/m2 per interval)
@@ -53,7 +58,7 @@ for (i in 1:length(survey.dates)) {
   dat <- cbind(time, date, lat, long, start_time, end_time, p)
   
   #append to current file
-  write.table(dat, file = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/kaos_combined_density_intervals.csv", row.names = F, sep = ",", append = TRUE)
+  write.table(dat, file = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/kaos_combined_density_intervals.csv", row.names = F, col.names = F, sep = ",", append = TRUE)
   
   msg <- paste("Finished calculating krill density for date ", survey.dates[i], sep = "")
   message(msg)
