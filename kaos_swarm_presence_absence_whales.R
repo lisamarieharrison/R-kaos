9b66df8c086c280ee692dbab1f7d64c8aa0cc852
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-01-30 12:24:42 +1100

    Same as kaos_swarm_presence_absence.R but for whales instead of Adelie penguins.

---
+++ kaos_swarm_presence_absence_whales.R
@@ -0,0 +1,58 @@
+#presence and absence of kaos swarms linked to predator presence-absence for whales
+#Author: Lisa-Marie Harrison
+#Date: 29/01/2015
+
+setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/schools detection")
+kaos_dates <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/kaos_survey_dates_sd.csv", header = F)$V1
+library(chron)
+
+for(i in 1:length(kaos_dates)) {
+  
+  high <- read.csv(paste("high_aggregations_by_region", kaos_dates[i], ".csv", sep = ""), header= T)
+  med <- read.csv(paste("medium_aggregations_by_region", kaos_dates[i], ".csv", sep = ""), header = T)
+  low <- read.csv(paste("low_aggregations_by_region", kaos_dates[i], ".csv", sep = ""), header= T)
+  pred <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/kaos_cetaceans.csv")  
+  
+  pred <- pred[pred$date == kaos_dates[i], ]
+  
+  #exclude other dates
+  low <- low[low$Date_S == kaos_dates[i] & low$Date_E == kaos_dates[i], ]
+  med <- med[med$Date_S == kaos_dates[i] & med$Date_E == kaos_dates[i], ]
+  high <- high[high$Date_S == kaos_dates[i] & high$Date_E == kaos_dates[i], ]
+  
+  
+  
+  high$Time_S <- chron(times. = high$Time_S, format = "h:m:s")
+  high$Time_E <- chron(times. = high$Time_E, format = "h:m:s")
+  med$Time_S <- chron(times. = med$Time_S, format = "h:m:s")
+  med$Time_E <- chron(times. = med$Time_E, format = "h:m:s")
+  low$Time_S <- chron(times. = low$Time_S, format = "h:m:s")
+  low$Time_E <- chron(times. = low$Time_E, format = "h:m:s")
+  
+  
+  plot(c(high$Time_S[1], high$Time_E[1]), c(3, 3), type = "l", xlim = c(min(low$Time_S), max(low$Time_E)), lwd = 2, xaxt = "n", ylim = c(0, 4), yaxt = "n", xlab = "time", ylab = "swarm")
+  for (j in 1:nrow(low)) {
+    points(c(high$Time_S[j], high$Time_E[j]), c(3, 3), type = "l", lwd = 5, col = "red")
+    points(c(med$Time_S[j], med$Time_E[j]), c(2, 2), type = "l", lwd = 5, col = "orange")
+    points(c(low$Time_S[j], low$Time_E[j]), c(1, 1), type = "l", lwd = 5)
+    
+  }
+  title(paste("Date", kaos_dates[i]))
+  
+  axis(1, at = seq(from = min(low$Time_S), to = max(low$Time_E), length.out = 10), 
+       labels = chron(times. = seq(from = chron(times. = min(low$Time_S), format = "h:m:S"), to = chron(times. = max(low$Time_E), format = "h:m:s"), length.out = 10)))
+  axis(2, at = c(1:3), labels = c("low", "medium", "high"))
+  
+  
+  pred$time <- chron(times. = pred$time, format = "h:m:s")
+  
+  if (nrow(pred) != 0) {
+    for (j in 1:nrow(pred)) {
+      
+      points(pred$time[j], 4, pch = "x", col = "darkgreen", cex = 3)
+      
+    }
+  }
+  
+}
+
