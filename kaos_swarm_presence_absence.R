03c1a5127d90dee5aadac2f8c47e94e9575a883a
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-04 13:32:20 +1100

    Increased line thickness in plots.

--- kaos_swarm_presence_absence.R
+++ kaos_swarm_presence_absence.R
@@ -34,13 +34,13 @@ for(i in 1:length(kaos_dates)) {
   #plot krill swarm location at each density by depth and time  
   plot(c(krill["high"][[1]]$Time_S[1], krill["high"][[1]]$Time_E[1]), c(krill["high"][[1]]$Depth_mean[1], krill["high"][[1]]$Depth_mean[1]), type = "l", xlim = c(min(krill["low"][[1]]$Time_S), max(krill["low"][[1]]$Time_E)), lwd = 2, xaxt = "n", ylim = c(250, 0), yaxt = "n", xlab = "time", ylab = "swarm", col = "white")
   for (j in 1:nrow(low)) {
-    points(c(krill["low"][[1]]$Time_S[j], krill["low"][[1]]$Time_E[j]), c(krill["low"][[1]]$Depth_mean[j], krill["low"][[1]]$Depth_mean[j]), type = "l", lwd = 5, cex = 2)
+    points(c(krill["low"][[1]]$Time_S[j], krill["low"][[1]]$Time_E[j]), c(krill["low"][[1]]$Depth_mean[j], krill["low"][[1]]$Depth_mean[j]), type = "l", lwd = 8)
    }
   for (j in 1:nrow(low)) {
-    points(c(krill["med"][[1]]$Time_S[j], krill["med"][[1]]$Time_E[j]), c(krill["med"][[1]]$Depth_mean[j], krill["med"][[1]]$Depth_mean[j]), type = "l", lwd = 5, col = "darkorange", cex = 2)
+    points(c(krill["med"][[1]]$Time_S[j], krill["med"][[1]]$Time_E[j]), c(krill["med"][[1]]$Depth_mean[j], krill["med"][[1]]$Depth_mean[j]), type = "l", lwd = 8, col = "darkorange")
   }
   for (j in 1:nrow(low)) {
-     points(c(krill["high"][[1]]$Time_S[j], krill["high"][[1]]$Time_E[j]), c(krill["high"][[1]]$Depth_mean[j], krill["high"][[1]]$Depth_mean[j]), type = "l", lwd = 5, col = "red", cex = 2)    
+     points(c(krill["high"][[1]]$Time_S[j], krill["high"][[1]]$Time_E[j]), c(krill["high"][[1]]$Depth_mean[j], krill["high"][[1]]$Depth_mean[j]), type = "l", lwd = 8, col = "red")    
   }
   title(paste("Date", kaos_dates[i]))
   
