72914611c16f5f8f95faf08cf4a8e6024f794dde
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-04 15:23:08 +1000

    Same as for 50ping density but for 500m data.

--- kaos_calculate_density_500m.R
+++ kaos_calculate_density_500m.R
@@ -1,17 +1,17 @@
 #read in each kaos integration by cells file and calculate density for each integration interval
 #write into a single file
-#specify maximum depth layer to integrate to
 #Author: Lisa-Marie Harrison
-#Date: 18/10/2014
+#Date: 01/10/2014
 
-setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/exported_integrations_500m")
+setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS/exported_integrations_500m")
+
+file.create("C:/Users/Lisa/Documents/phd/southern ocean/KAOS/kaos_combined_density_intervals_500m.csv")
 
 #get dates of files in folder
-survey.dates <- as.numeric(substr(list.files(path = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/exported_integrations_500m", pattern = paste("(", "38kHz", ").*\\.csv$", sep = ""), full.names = F), start = 24, stop = 31))
-
-depth_layer <- 6
+survey.dates <- as.numeric(substr(list.files(path = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS/exported_integrations_500m", pattern = paste("(", "38kHz", ").*\\.csv$", sep = ""), full.names = F), start = 24, stop = 31))
 
 #for each date, read in the 38 and 120kHz files and process the data
+
 for (i in 1:length(survey.dates)) {
   
   
@@ -27,16 +27,12 @@ for (i in 1:length(survey.dates)) {
   acoustic_38 <- acoustic_38[acoustic_38$Layer > 0, ]
   acoustic_120 <- acoustic_120[acoustic_120$Layer > 0, ]  
   
-  #remove values at less than specified depth 
-  acoustic_38 <- acoustic_38[acoustic_38$Layer <= depth_layer, ]
-  acoustic_120 <- acoustic_120[acoustic_120$Layer <= depth_layer, ]  
-  
-  
   #calculate 120kHz - 38kHz for each 10x50 window
   sv_38 <- acoustic_38$Sv_mean
   sv_120 <- acoustic_120$Sv_mean
   sv_38[sv_38 > 500 | sv_38 < -500] <- NA
   sv_120[sv_120 > 500 | sv_120 < -500] <- NA
+  noise <- is.na(sv_120)
   sv_diff <- sv_120 - sv_38
   
   time  <- as.character(acoustic_38$Time_M[seq(1, nrow(acoustic_38), by = max(acoustic_38$Layer))])
@@ -56,22 +52,53 @@ for (i in 1:length(survey.dates)) {
   mvbs  <- 10*log10(aggregate(matrix(sv, ncol = 1), by = list(rep(c(1:(length(sv)/max(acoustic_38$Layer))), each = max(acoustic_38$Layer))), sum, na.rm = T)$V1/max(acoustic_38$Layer))
   mvbs[mvbs == -Inf] <- NA
   
+  
   #convert to density using target strength (kg/m2 per interval)
-  p <- 30*10 ^((mvbs - -42.22)/10)*1000
+  p <- 250*10 ^((mvbs - -42.22)/10)*1000
   
-  #plot density (kg/m2 per interval) along the transect
-  plot(ksmooth(c(1:length(p)), p, bandwidth = 15), type = "l")
+  #add zero krill intervals back in
+  p[is.na(p)] <- 0
   
-  dat <- cbind(time, date, lat, long, start_time, end_time, p)
+  #remove noise intervals
+  p[table(acoustic_120$Interval, noise)[, 1] == 49] <- NA
+  
+  #calculate interval length (m)
+  interval_length <- 0
+  for (k in 1:length(unique(acoustic_38$Interval))) {
+    interval_length[k] <- acoustic_38$Dist_E[acoustic_38$Layer == 1 & acoustic_38$Interval == unique(acoustic_38$Interval)[k]] - acoustic_38$Dist_S[acoustic_38$Layer == 1 & acoustic_38$Interval == unique(acoustic_38$Interval)[k]]
+  }
+  
+  #calculate interval weighting
+  interval_weight <- interval_length/sum(interval_length)
+  
+  #calculate mean weighted density
+  transect_mean <- sum(na.omit(p*interval_weight))
+    
+  dat <- cbind(time, date, lat, long, start_time, end_time, p, interval_weight, interval_length)
   
   #append to current file
-  write.table(dat, file = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS data/kaos_combined_density_intervals_500m.csv", row.names = F, col.names = F, sep = ",", append = TRUE)
+  write.table(dat, file = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS/kaos_combined_density_intervals_500m.csv", row.names = F, col.names = F, sep = ",", append = TRUE)
   
   msg <- paste("Finished calculating krill density for date ", survey.dates[i], sep = "")
   message(msg)
   
 }
 
+#add names to columns
+dat <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/KAOS/kaos_combined_density_intervals_500m.csv", header = F)
+names(dat) <- c("time", "date", "lat", "long", "start_time", "end_time", "p", "interval_weight", "interval_length")
+write.csv(dat, "C:/Users/Lisa/Documents/phd/southern ocean/KAOS/kaos_combined_density_intervals_500m.csv", row.names = F)
 
+dat <- read.csv("C:/Users/Lisa/Documents/phd/southern ocean/KAOS/kaos_combined_density_intervals_500m.csv", header = T)
+dat$interval_length[dat$interval_length > 500] <- 0
+dat$p[dat$p > 5000] <- NA
 
+transect_density <- 0
+for(i in 1:length(unique(dat$date))) {
+  transect_density[i] <- sum(na.omit(dat$p[dat$date == unique(dat$date)[i]]*dat$interval_weight[dat$date == unique(dat$date)[i]]))
+}
 
+sum(na.omit(dat$p*dat$interval_length/sum(dat$interval_length)))
+
+#calculate biomass (kt)
+(sum(na.omit(dat$p*dat$interval_length/sum(dat$interval_length)))*10500e6)/1000/1000/1000
