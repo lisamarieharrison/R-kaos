78b6dedff28a072610c655313b8a50bcce70786d
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-18 15:37:25 +1000

    Added lines to pull out date and time for each ctd station

--- kaos-ctd-combine.R
+++ kaos-ctd-combine.R
@@ -3,7 +3,7 @@
 #date: 18/06/2015
 
 
-setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/kaos_ctd/")
+setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/kaos/kaos_ctd/")
 ctd_files <- list.files(pattern = ".all")
 
 file.create("kaos_ctd.csv")
@@ -12,7 +12,13 @@ for (i in ctd_files) {
   
   dat <- read.table(i, skip = 15, header = F)
   
-  out <- cbind(dat, rep(which(ctd_files == i), nrow(dat)))
+  time <- read.csv(i, header = F)
+  
+  date <- substr(time[3, 1], start = 21, stop = 31)
+  start_time <- substr(time[4, 1], start = 21, stop = 26)
+  bottom_time <- substr(time[5, 1], start = 21, stop = 26)
+    
+  out <- cbind(dat, rep(which(ctd_files == i), nrow(dat)), rep(start_time, nrow(dat)), rep(bottom_time, nrow(dat)), rep(date, nrow(dat)))
   
   write.table(out, "kaos_ctd.csv", row.names = F, col.names = F, sep = ",", append = T)
   
@@ -20,5 +26,5 @@ for (i in ctd_files) {
 
 #add headers to the completed file
 dat <- read.csv("kaos_ctd.csv", header = F)
-names(dat) <- c("depth", "temp", "cond", "sal", "oxy", "fluoro", "npts", "stn")
+names(dat) <- c("depth", "temp", "cond", "sal", "oxy", "fluoro", "npts", "stn", "start_time", "bottom_time", "date")
 write.csv(dat, "kaos_ctd.csv", row.names = F)
