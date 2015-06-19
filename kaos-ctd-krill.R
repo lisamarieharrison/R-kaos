9b04ef7248d0a0093f7e7d27924f586cfcc95b00
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-19 14:57:52 +1000

    Changed to make the values assigned to the nearest even depth.

--- kaos-ctd-krill.R
+++ kaos-ctd-krill.R
@@ -13,17 +13,46 @@ ctd <- ctd[ctd$stn %in% unique(krill$stn), ]
 
 ctd <- ctd[ctd$depth <= 250, ]
 
+#remove error values
+ctd$fluoro[ctd$fluoro == -9] <- NA
+
+#remove station 11 because no krill data
+ctd <- ctd[ctd$stn != 11, ]
+krill <- krill[krill$stn != 11, ]
+
+krill$p[is.na(krill$p)] <- 0
+
+krill <- krill[krill$depth > 0, ]
+krill <- krill[!is.na(krill$depth), ]
+
 #pad krill to 5m depths
 p <- rep(NA, nrow(ctd))
 for (i in unique(ctd$stn)) {
-  p[ctd$stn == i][seq(1, sum(ctd$stn == i), by = 2)][1:49] <- krill$p[krill$stn == i]
+  p[ctd$stn == i & ctd$depth %in% (2*round(krill$depth/2)[krill$stn == i])] <- krill$p[krill$stn == i]
 }
+p[p == 0] <- NA
 
-lat.plot <- xyplot(ctd$fluoro + p ~ ctd$depth | ctd$stn,
+lat.plot <- xyplot(log(ctd$fluoro + 1) ~ ctd$depth | ctd$stn,
                    ylab = "fluoro", outer = FALSE, type = "l")
-lat.plot2 <- xyplot(p*2 ~ ctd$depth | ctd$stn,
+lat.plot2 <- xyplot(krill$p + 2 ~ krill$depth | krill$stn,
                    ylab = "fluoro", outer = FALSE, type = "p", col = "red", pch = 19)
 lat.plot + lat.plot2
+
+
+#linear model for log-log krill fluoro relationship
+fluoro.lm <- lm(log(p) ~ log(ctd$fluoro + 1))
+summary(fluoro.lm)
+
+
+plot(log(ctd$fluoro + 1), log(p), xlab = "log(fluoro)", ylab = "log(krill density)")
+y <- fluoro.lm$coefficients[1] + fluoro.lm$coefficients[2]*log(ctd$fluoro + 1)
+x <- log(ctd$fluoro + 1)
+xy <- cbind(x, y)
+xy <- xy[order(xy[, 1]), ]
+points(xy[, 1], xy[, 2], col = "red", type = "l")
+
+
+
 
 
 
