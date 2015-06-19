2c4de01ebcdc5795bff24353d688d4e605247a1a
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-19 12:30:01 +1000

    Added kaos krill data. Currently in 5m bins so thinned to correct depths

--- kaos-ctd-krill.R
+++ kaos-ctd-krill.R
@@ -4,10 +4,26 @@
 
 setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/KAOS/")
 ctd <- read.csv("kaos_ctd.csv", header = T)
+krill <- read.csv("kaos_krill_ctd.csv", header = T)
 library(lattice)
+library(latticeExtra)
 
-ctd <- ctd[ctd$depth <= 500, ]
+#remove stations where there was no acoustic data
+ctd <- ctd[ctd$stn %in% unique(krill$stn), ]
 
-lat.plot <- xyplot(ctd$fluoro ~ ctd$depth | ctd$stn,
+ctd <- ctd[ctd$depth <= 250, ]
+
+#pad krill to 5m depths
+p <- rep(NA, nrow(ctd))
+for (i in unique(ctd$stn)) {
+  p[ctd$stn == i][seq(1, sum(ctd$stn == i), by = 2)][1:49] <- krill$p[krill$stn == i]
+}
+
+lat.plot <- xyplot(ctd$fluoro + p ~ ctd$depth | ctd$stn,
                    ylab = "fluoro", outer = FALSE, type = "l")
-update(lat.plot)
+lat.plot2 <- xyplot(p*2 ~ ctd$depth | ctd$stn,
+                   ylab = "fluoro", outer = FALSE, type = "p", col = "red", pch = 19)
+lat.plot + lat.plot2
+
+
+
